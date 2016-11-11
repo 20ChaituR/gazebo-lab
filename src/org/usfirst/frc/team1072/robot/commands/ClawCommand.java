fe2648b6c1f43fbcea5b29fe72c1c0cfe5d53031
20kylel <20kylel@students.harker.org>
2016-11-10 17:05:28 -0800

    Claw Changes
    Edited execute and isFinished in Claw Command

--- src/org/usfirst/frc/team1072/robot/commands/ClawCommand.java
+++ src/org/usfirst/frc/team1072/robot/commands/ClawCommand.java
@@ -9,8 +9,7 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class ClawCommand extends Command{
 
-	private double speed;
-	private boolean openvar;
+	private static boolean openvar;
 	
 	public ClawCommand() {
 		requires(Robot.claw);
@@ -20,24 +19,27 @@ public class ClawCommand extends Command{
 	protected void end() {
 		// TODO Auto-generated method stub
 		//finish up, set claw speed to 0
+		Robot.claw.setSpeed(0.0);
 	}
 
 	@Override
 	protected void execute() {
 		// TODO Auto-generated method stub
-		if (Robot.oi.getGp().getButtonX().get()) {
-			if (!(Robot.claw.getClawVictor().getPosition() >= 1 - Robot.claw.getDeadzone() && Robot.claw.getClawVictor().getPosition() <= 1)) {
-				Robot.claw.setSpeed(0.5);
-			}
-		} else {
-			Robot.claw.setSpeed(-speed);
-		}
+		
 	}
 
 	@Override
 	protected void initialize() {
 		// TODO Auto-generated method stub
 		//set claw speed to opening/closing depending on current state
+		if (openvar) {
+			openvar = false;
+			Robot.claw.setSpeed(-0.5);
+		} else {
+			openvar = true;
+			Robot.claw.setSpeed(0.5);
+		}
+		
 	}
 
 	@Override
@@ -50,6 +52,13 @@ public class ClawCommand extends Command{
 	protected boolean isFinished() {
 		// TODO Auto-generated method stub
 		//check if the claw is at maximum
+		if (openvar) {
+			if (Robot.claw.getClawVictor().getPosition() == 1) {
+				return true;
+			}
+		} else if (Robot.claw.getClawVictor().getPosition() == 0) {
+			return true;
+		}
 		return false;
 	}
 
